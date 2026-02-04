97a483d22d302306c64d2d4e303b9363e3327239
Vlada <vladislavaskripcenko@gmail.com>
2026-02-04 11:28:11 +0500

    bla bla bla bla

--- src/hw3.java
+++ src/hw3.java
@@ -2,5 +2,6 @@ public class hw3 {
 
     public void foo() {
         System.out.println("bla bla bla / i never say bla bla bla/ foo kakaya gadost");
+        System.out.println("bla bla bla / i never say bla bla bla/ foo kakaya gadost");
     }
 }
